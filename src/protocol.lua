d3d28fce01c94498d4c67a0f58845bbac7a4510d
Maddie <52103563+maddie480@users.noreply.github.com>
2026-08-29 21:59:45 +0200

    Fix 1-click install links

--- src/protocol.lua
+++ src/protocol.lua
@@ -1,5 +1,12 @@
 return function(url, autoclose)
     if url:match("https?://") then
+        -- the URL is like: https://gamebanana.com/mmdl/1414214,Mod,424541
+        -- and we need to truncate it
+        local endIndex = url:find(',')
+        if endIndex then
+            url = url:sub(1, endIndex - 1)
+        end
+
         return require("modinstaller").install(url, nil, nil, autoclose)
     end
 
